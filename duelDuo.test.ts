874f07b0ba799c158a1597484af57cfaf3ed37d4
Nichole Boothman <nbooth731@gmail.com>
2021-12-17 12:53:45 -0500

    all tests complete automation complete

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -17,4 +17,25 @@ test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
+
+    await driver.sleep(3000)
+})
+
+test('draw button displays choices', async () => {
+    await driver.findElement(By.id('draw')).click()
+    const view = await driver.findElement(By.id('choices'))
+    const displayed = await view.isDisplayed()
+    expect(displayed).toBe(true)
+    
+    await driver.sleep(3000)
+})
+
+test('add to duo adds cards', async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.findElement(By.css('[class= "bot-btn"]')).click()
+    const duoHeader = await driver.findElement(By.id('your-duo-header'))
+    const displayed = await duoHeader.isDisplayed()
+    expect(displayed).toBe(true)
+
+    await driver.sleep(3000)
 })
