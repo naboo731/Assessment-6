4f7f58fbce6bee84ff066ab8808e0162c56d86dc
Nichole Boothman <nbooth731@gmail.com>
2021-12-17 12:25:18 -0500

    testing complete

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,15 @@
 const {shuffleArray} = require('./utils')
 
+
 describe('shuffleArray should', () => {
-    test()
+    test('shuffleArray should return an array', () => {
+        let argArr = [1,2,3,4,5,6,7,8]
+        let returnArr = shuffleArray(argArr)
+        expect(typeof returnArr).toEqual('array')
+    }), 
+    test('shuffleArray return length is equal to argument length', () => {
+        let argArr = [1,2,3,4,5,6,7,8]
+        let returnArr = shuffleArray(argArr)
+        expect(returnArr.length).toEqual(argArr.length)
+    })
 })
